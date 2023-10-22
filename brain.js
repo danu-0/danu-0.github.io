60a186fa27028ae8441c3c5b23853050d033c2dd
unknown <danuhaeridaputra07@gmail.com>
2023-10-22 21:25:29 +0700

    first commit

---
+++ brain.js
@@ -0,0 +1,7 @@
+document.getElementById('buton').addEventListener('click', function(){
+    window.open('https://genshin-impact.fandom.com/wiki/Raiden_Shogun')
+});
+const ig = document.getElementsById("pngig");
+ig.addEventListener("click", function(){
+    alert('image')
+})
