64774514bd0392cde6c7ab473c5582e1d73c86d9
unknown <danuhaeridaputra07@gmail.com>
2023-11-14 18:51:28 +0700

    added video embed

--- brain.js
+++ brain.js
@@ -5,3 +5,18 @@ const ig = document.getElementsById("pngig");
 ig.addEventListener("click", function(){
     alert('image')
 })
+
+function igLink() {
+    window.open('https://www.instagram.com/mihoyo.impact/?hl=id', '_blank')
+}
+function fbLink() {
+    window.open("https://www.facebook.com/GenshinImpact.id/photos/a.2483187211961252/3371527083127256/?type=3", '_blank')
+}
+
+function ytLink() {
+    window.open("https://www.youtube.com/hashtag/hoyoverse", '_blank')
+}
+
+function twtLink() {
+    window.open("https://mobile.twitter.com/hoyovers", '_blank')
+}
